48484afb05f38c37e7b6610b4fbe3ebcf761f345
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-07 09:48:11 +0200

    Init spring

---
+++ src/main/java/com/github/s1ckcode/SalesManagement/SalesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.s1ckcode.SalesManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/s1ckcode/SalesManagement/SalesManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.s1ckcode.SalesManagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SalesManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
