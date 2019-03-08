4b763d79cde6439e4e75c8c99d50f963b10004c3
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-08 11:49:44 +0200

    Add: interface UserRepository that extends CrudRepository

---
+++ src/main/java/com/github/s1ckcode/SalesManagement/UserRepository.java
@@ -0,0 +1,6 @@
+package com.github.s1ckcode.SalesManagement;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Integer> {
+}
