49e45ea53f0d322ffe4bb103913f05ee77e761b0
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-08 12:06:51 +0200

    Add: Some dummy data to test database

--- src/main/java/com/github/s1ckcode/SalesManagement/SalesManagementApplication.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/SalesManagementApplication.java
@@ -1,13 +1,26 @@
 package com.github.s1ckcode.SalesManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class SalesManagementApplication {
+public class SalesManagementApplication implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesManagementApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("Jaska", LocalDate.now(),"Salesperson","Hash1"));
+		userRepository.save(new User("Tintti",LocalDate.now(),"Office dog","Hash2"));
+	}
 }
