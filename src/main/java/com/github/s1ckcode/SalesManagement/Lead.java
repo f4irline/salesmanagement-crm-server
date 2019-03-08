55df53112746150f51907c49a1e5537bd7d205fc
Elias <elias.pohjalainen@tuni.fi>
2019-03-08 13:13:07 +0200

    Lead.java created

---
+++ src/main/java/com/github/s1ckcode/SalesManagement/Lead.java
@@ -0,0 +1,100 @@
+package com.github.s1ckcode.SalesManagement;
+
+import java.sql.Date;
+
+public class Lead {
+
+    private int id;
+    private String company;
+    private String contact;
+    private int phone;
+    private String email;
+    private String website;
+
+    public Lead() {
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the company
+     */
+    public String getCompany() {
+        return company;
+    }
+
+    /**
+     * @param company the company to set
+     */
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    /**
+     * @return the contact
+     */
+    public String getContact() {
+        return contact;
+    }
+
+    /**
+     * @param contact the contact to set
+     */
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    /**
+     * @return the phone
+     */
+    public int getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the website
+     */
+    public String getWebsite() {
+        return website;
+    }
+
+    /**
+     * @param website the website to set
+     */
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}
