f5ab369208af87b88e6eb0852877256442277616
Elias <elias.pohjalainen@tuni.fi>
2019-03-08 12:49:43 +0200

    Event.java created (for real)

---
+++ src/main/java/com/github/s1ckcode/SalesManagement/Event.java
@@ -0,0 +1,71 @@
+package com.github.s1ckcode.SalesManagement;
+
+import java.sql.Date;
+
+public class Event {
+
+    private int id;
+    private String type;
+    private String notes;
+    private Date date;
+
+    public Event() {
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the notes
+     */
+    public String getNotes() {
+        return notes;
+    }
+
+    /**
+     * @param notes the notes to set
+     */
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
