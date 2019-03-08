f42ffea355fd36d943d094941b0a657ff751743d
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-08 13:58:24 +0200

    Add: findByName()

--- src/main/java/com/github/s1ckcode/SalesManagement/UserRepository.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/UserRepository.java
@@ -2,5 +2,8 @@ package com.github.s1ckcode.SalesManagement;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User,Integer> {
+    public User findByName(String name);
 }
