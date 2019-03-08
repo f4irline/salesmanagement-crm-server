ac1f0c9567917178992127e4331bb6c0be722240
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-08 11:48:34 +0200

    Add: variables id, name and constructors to User

--- src/main/java/com/github/s1ckcode/SalesManagement/User.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/User.java
@@ -1,4 +1,27 @@
 package com.github.s1ckcode.SalesManagement;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public User(){}
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
