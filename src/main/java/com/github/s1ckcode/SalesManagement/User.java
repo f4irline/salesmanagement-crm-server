955c21a45deedbc373779ae49552ae08321b435d
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-08 12:03:02 +0200

    Add: New variables and getter&setters

--- src/main/java/com/github/s1ckcode/SalesManagement/User.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/User.java
@@ -3,6 +3,7 @@ package com.github.s1ckcode.SalesManagement;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
 public class User {
@@ -10,11 +11,18 @@ public class User {
     @GeneratedValue
     private int id;
     private String name;
+    private LocalDate lastLogin;
+    private String role;
+    private String password;
+    private final LocalDate createDate = LocalDate.now();
 
     public User(){}
 
-    public User(String name) {
+    public User(String name, LocalDate lastLogin, String role, String password) {
         this.name = name;
+        this.lastLogin = lastLogin;
+        this.role = role;
+        this.password = password;
     }
 
     public void setName(String name){
@@ -23,5 +31,33 @@ public class User {
 
     public String getName() {
         return name;
+    }
+
+    public LocalDate getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDate lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
     }
 }
