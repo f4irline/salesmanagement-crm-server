5f08dcedf849ec0d160fb8aea698fe2d7f9daf58
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-14 12:24:32 +0200

    Add: Utils.java with help methods

---
+++ src/main/java/com/github/s1ckcode/SalesManagement/Utils.java
@@ -0,0 +1,20 @@
+package com.github.s1ckcode.SalesManagement;
+
+import com.github.s1ckcode.SalesManagement.User.User;
+
+public class Utils {
+
+    public static double getHitrate(User user) {
+        return 0.0;
+    }
+
+    public static double getAvgSales(User user) {
+        return 0.0;
+    }
+
+    public static double getAllSales(User user) {
+        return 0.0;
+    }
+
+
+}
