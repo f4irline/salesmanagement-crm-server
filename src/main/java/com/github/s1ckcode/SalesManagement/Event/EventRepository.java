98d2adba8e60d8226ba6a747996e18f6f94176e3
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-14 12:48:56 +0200

    Change: findEventsByUser now gets Optional<User> as a parameter

--- src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
@@ -3,6 +3,8 @@ package com.github.s1ckcode.SalesManagement.Event;
 import com.github.s1ckcode.SalesManagement.User.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends CrudRepository<Event,Integer> {
-    public Iterable<Event> findEventsByUser(User user);
+    public Iterable<Event> findEventsByUser(Optional<User> user);
 }
