7f0b597fbcaf6b16056c73b0ce0b179384c40b00
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-14 12:58:20 +0200

    Add: hashMyPassword()

--- src/main/java/com/github/s1ckcode/SalesManagement/Utils.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Utils.java
@@ -1,6 +1,7 @@
 package com.github.s1ckcode.SalesManagement;
 
 import com.github.s1ckcode.SalesManagement.User.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Utils {
 
@@ -14,6 +15,11 @@ public class Utils {
 
     public static double getAllSales(User user) {
         return 0.0;
+    }
+
+    public static String hashMyPassword(String password) {
+        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+        return bCryptEncoder.encode(password);
     }
 
 
