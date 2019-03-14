bc446e9093db4ca850162fc15a418bcf4c7e3c21
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-14 14:40:16 +0200

    Add: findEventsByEventTypeAndUser()

--- src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface EventRepository extends CrudRepository<Event,Integer> {
     public Iterable<Event> findEventsByUser(Optional<User> user);
+    public Iterable<Event> findEventsByEventTypeAndUser(int eventType, Optional<User> user);
 }
