3f6a61945dc4f7d18e984602afb7007514351b6c
Elias <elias.pohjalainen@tuni.fi>
2019-03-14 14:21:24 +0200

    Utils.java utility methods implemented

--- src/main/java/com/github/s1ckcode/SalesManagement/Utils.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Utils.java
@@ -1,20 +1,76 @@
 package com.github.s1ckcode.SalesManagement;
 
+import com.github.s1ckcode.SalesManagement.Event.Event;
+import com.github.s1ckcode.SalesManagement.Event.EventRepository;
 import com.github.s1ckcode.SalesManagement.User.User;
+import com.github.s1ckcode.SalesManagement.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.github.s1ckcode.SalesManagement.Event.Event.CONTACT;
+import static com.github.s1ckcode.SalesManagement.Event.Event.SALE;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Utils {
 
-    public static double getHitrate(User user) {
-        return 0.0;
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    public double getHitrate(Optional<User> user) {
+
+        double contacts = 0;
+        double sales = 0;
+
+        //user.ifPresent();
+
+        Iterable<Event> events = (eventRepository.findEventsByUser(user));
+
+        for(Event event:events) {
+            if(event.getEventType() == SALE) {
+                sales++;
+            }
+            else if(event.getEventType() == CONTACT) {
+                contacts++;
+            }
+        }
+
+        return sales / contacts;
     }
 
-    public static double getAvgSales(User user) {
-        return 0.0;
+    public double getAvgSales(Optional<User> user) {
+
+        double salesCount = 0;
+        double salesValue = 0;
+
+        Iterable<Event> events = (eventRepository.findEventsByUser(user));
+
+        for(Event event:events) {
+            if(event.getEventType() == SALE) {
+                salesCount++;
+                salesValue =+ event.getSum();
+            }
+        }
+
+        return salesValue / salesCount;
     }
 
-    public static double getAllSales(User user) {
-        return 0.0;
+    public double getAllSales(Optional<User> user) {
+
+        double salesValue = 0;
+
+        Iterable<Event> events = (eventRepository.findEventsByUser(user));
+
+        for(Event event:events) {
+            if(event.getEventType() == SALE) {
+                salesValue =+ event.getSum();
+            }
+        }
+
+        return salesValue;
     }
 
     public static String hashMyPassword(String password) {
