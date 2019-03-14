ffb8bb647ca5eb3c068322a56bc4ef8f9a53a232
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-14 12:24:09 +0200

    Add: addUser() and deleteUser()

--- src/main/java/com/github/s1ckcode/SalesManagement/MyRestController.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/MyRestController.java
@@ -1,5 +1,6 @@
 package com.github.s1ckcode.SalesManagement;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.github.s1ckcode.SalesManagement.User.User;
 import com.github.s1ckcode.SalesManagement.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,21 @@ public class MyRestController {
     @Autowired
     UserRepository userRepository;
 
-    @RequestMapping(value="/user/{name}", method = RequestMethod.GET)
+    @GetMapping(value="/user/{name}")
     public User user(@PathVariable String name) {
         return userRepository.findByName(name);
     }
+
+    @PostMapping(value = "/user/add")
+    public void addUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+    @DeleteMapping(value = "user/{userId}")
+    public void deleteUser(@PathVariable int userId) {
+        userRepository.deleteById(userId);
+    }
+
+
+
 }
