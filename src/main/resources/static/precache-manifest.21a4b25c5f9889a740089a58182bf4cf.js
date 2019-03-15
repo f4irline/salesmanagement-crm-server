d7c673a3ab6f74c1d6b7128bb59a48668720969c
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-15 16:14:11 +0200

    Add reactjs frontend to static pages

---
+++ src/main/resources/static/precache-manifest.21a4b25c5f9889a740089a58182bf4cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd81cc3b3ad8fc05d782",
+    "url": "/static/js/main.dd81cc3b.chunk.js"
+  },
+  {
+    "revision": "6e0b11f65a5c3662c610",
+    "url": "/static/js/1.6e0b11f6.chunk.js"
+  },
+  {
+    "revision": "dd81cc3b3ad8fc05d782",
+    "url": "/static/css/main.f4c98ffe.chunk.css"
+  },
+  {
+    "revision": "ba5185063889c197fc3b0b03a11c030f",
+    "url": "/index.html"
+  }
+];
