c9bde78aa332872ec1ff1c077eb1f0e7862ae8a3
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-15 16:48:41 +0200

    Try to fix permission issues with Heroku

---
+++ src/main/resources/static/precache-manifest.54401a74fa7f8c784155f950e79971d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65282180f68734d78a95",
+    "url": "/static/js/main.65282180.chunk.js"
+  },
+  {
+    "revision": "6e0b11f65a5c3662c610",
+    "url": "/static/js/1.6e0b11f6.chunk.js"
+  },
+  {
+    "revision": "65282180f68734d78a95",
+    "url": "/static/css/main.f4c98ffe.chunk.css"
+  },
+  {
+    "revision": "f762f2f4a7c5b1a48c59914626d1c5ab",
+    "url": "/index.html"
+  }
+];
