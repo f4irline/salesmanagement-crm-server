346dd0d5722a9f029f2071775b6762426df8c6ed
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-15 16:26:51 +0200

    Try to fix permission issues with Heroku

--- src/main/java/com/github/s1ckcode/SalesManagement/MyConfiguration.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/MyConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-
 public class MyConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
---
+++ src/main/java/com/github/s1ckcode/SalesManagement/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.github.s1ckcode.SalesManagement;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
+    }
+}
