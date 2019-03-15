34100856ac70fbd7e0dc14cd1d316c46add0f409
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-15 14:52:34 +0200

    Add: needed methods

--- src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
@@ -3,9 +3,13 @@ package com.github.s1ckcode.SalesManagement.Event;
 import com.github.s1ckcode.SalesManagement.User.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Optional;
 
 public interface EventRepository extends CrudRepository<Event,Integer> {
     public Iterable<Event> findEventsByUser(User user);
     public Iterable<Event> findEventsByEventTypeAndUser(int eventType, User user);
+    public Iterable<Event> findEventsByDateBetween(LocalDate startDate, LocalDate endDate);
+    public Iterable<Event> findEventsByDateMonth(Month month);
 }
