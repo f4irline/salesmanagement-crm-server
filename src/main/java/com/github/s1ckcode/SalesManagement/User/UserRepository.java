7d0fbe87e4cc5b2e8177146e19e4a3a1d9db7d76
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-15 10:41:22 +0200

    Delete: findByName()

--- src/main/java/com/github/s1ckcode/SalesManagement/User/UserRepository.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/User/UserRepository.java
@@ -4,5 +4,4 @@ import com.github.s1ckcode.SalesManagement.User.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User,Integer> {
-    public User findByName(String name);
 }
