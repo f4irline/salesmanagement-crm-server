afe509bc4fcd09420a5c4e8956c8f91fd7344426
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-15 11:28:46 +0200

    Change: add lead for events

--- src/main/java/com/github/s1ckcode/SalesManagement/SalesManagementApplication.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/SalesManagementApplication.java
@@ -2,6 +2,7 @@ package com.github.s1ckcode.SalesManagement;
 
 import com.github.s1ckcode.SalesManagement.Event.Event;
 import com.github.s1ckcode.SalesManagement.Event.EventRepository;
+import com.github.s1ckcode.SalesManagement.Lead.Lead;
 import com.github.s1ckcode.SalesManagement.Lead.LeadRepository;
 import com.github.s1ckcode.SalesManagement.User.User;
 import com.github.s1ckcode.SalesManagement.User.UserRepository;
@@ -33,21 +34,25 @@ public class SalesManagementApplication implements CommandLineRunner {
 		userRepository.save(new User("Jaska","Jokunen", LocalDate.now(),User.ADMIN,"Hash1"));
 		userRepository.save(new User("Tintti","Ala-Hintti",LocalDate.now(),User.USER,"Hash2"));
 //		//User user, int eventType, String notes, LocalDate date
-		eventRepository.save(new Event(userRepository.findById(100001).get(), Event.CONTACT, "kontaktoitu asiakasta, on kiinnostunut",LocalDate.now()));
-		eventRepository.save(new Event(userRepository.findById(100001).get(), Event.CONTACT, "kontaktoitu asiakasta, on kiinnostunut",LocalDate.now()));
-		eventRepository.save(new Event(userRepository.findById(100001).get(), Event.CONTACT, "kontaktoitu asiakasta, on kiinnostunut",LocalDate.now()));
 
-        eventRepository.save(new Event(userRepository.findById(100001).get(), Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,01,01),200.0));
-		eventRepository.save(new Event(userRepository.findById(100001).get(), Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,02,02),100.0));
-		eventRepository.save(new Event(userRepository.findById(100001).get(), Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,03,03),150.0));
+        //LocalDate date, String companyName, String contactPerson, String contactRole, String phoneNumber, String email, String website, String industry, String notes
+        Lead lead = new Lead(LocalDate.now(), "CompanyABC","Keijo Ala-Kukkula","Boss Man", "010101010101", "keijukainen@koll.fi", "www.pekka.fi", "teollisuus", "Homopekka");
 
-        eventRepository.save(new Event(userRepository.findById(100002).get(), Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,01,01),10000.0));
-        eventRepository.save(new Event(userRepository.findById(100002).get(), Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,05,01),10600.0));
-        eventRepository.save(new Event(userRepository.findById(100002).get(), Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,06,01),10500.0));
+		eventRepository.save(new Event(userRepository.findById(100001).get(),lead, Event.CONTACT, "kontaktoitu asiakasta, on kiinnostunut",LocalDate.now()));
+		eventRepository.save(new Event(userRepository.findById(100001).get(),lead, Event.CONTACT, "kontaktoitu asiakasta, on kiinnostunut",LocalDate.now()));
+		eventRepository.save(new Event(userRepository.findById(100001).get(),lead, Event.CONTACT, "kontaktoitu asiakasta, on kiinnostunut",LocalDate.now()));
 
-        eventRepository.save(new Event(userRepository.findById(100002).get(), Event.MEETING, "tapaaminen sovittu kahvilaan",LocalDate.now(),"Kahvila kulman takana"));
-        eventRepository.save(new Event(userRepository.findById(100002).get(), Event.MEETING, "tapaaminen sovittu kahvilaan",LocalDate.now(),"Kahvila kulman takana"));
-        eventRepository.save(new Event(userRepository.findById(100002).get(), Event.MEETING, "tapaaminen sovittu kahvilaan",LocalDate.now(),"Kahvila kulman takana"));
+        eventRepository.save(new Event(userRepository.findById(100001).get(),lead, Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,01,01),200.0));
+		eventRepository.save(new Event(userRepository.findById(100001).get(),lead, Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,02,02),100.0));
+		eventRepository.save(new Event(userRepository.findById(100001).get(),lead, Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,03,03),150.0));
+
+        eventRepository.save(new Event(userRepository.findById(100002).get(),lead, Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,01,01),10000.0));
+        eventRepository.save(new Event(userRepository.findById(100002).get(),lead, Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,05,01),10600.0));
+        eventRepository.save(new Event(userRepository.findById(100002).get(),lead, Event.SALE, "kauppa tehty alennuksin",LocalDate.of(2019,06,01),10500.0));
+
+        eventRepository.save(new Event(userRepository.findById(100002).get(),lead, Event.MEETING, "tapaaminen sovittu kahvilaan",LocalDate.now(),"Kahvila kulman takana"));
+        eventRepository.save(new Event(userRepository.findById(100002).get(),lead, Event.MEETING, "tapaaminen sovittu kahvilaan",LocalDate.now(),"Kahvila kulman takana"));
+        eventRepository.save(new Event(userRepository.findById(100002).get(),lead, Event.MEETING, "tapaaminen sovittu kahvilaan",LocalDate.now(),"Kahvila kulman takana"));
 
 		//leadRepository.save(new Lead(eventRepository.findByEventId(1), "CompanyABC", "Contact123", "5069696969", "pekka@puu.com", "www.pekka.fi"));
 	}
