72b69e979b43487a205d35f1d260299bb0d5cd94
Tommi Lepola <lepola.tommi@gmail.com>
2019-03-21 18:08:11 +0200

    Add new version of the system to heroku

---
+++ src/main/resources/static/precache-manifest.b02456fc091056d71eed84be5bed1cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1864f66ccbfa01480637",
+    "url": "/static/js/main.1864f66c.chunk.js"
+  },
+  {
+    "revision": "cb8e1faced697c581fcc",
+    "url": "/static/js/1.cb8e1fac.chunk.js"
+  },
+  {
+    "revision": "1864f66ccbfa01480637",
+    "url": "/static/css/main.7ad96979.chunk.css"
+  },
+  {
+    "revision": "c7e3dd2eec5d2500bad13358f3b3e3bc",
+    "url": "/index.html"
+  }
+];
