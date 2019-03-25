ec694be2fd20e447e0175830c0868a721b4466b6
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-25 14:12:50 +0200

    ADD: MyRestController for getting data to front-end event-tables

--- src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Event/EventRepository.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 public interface EventRepository extends CrudRepository<Event,Integer> {
     public Iterable<Event> findEventsByUser(User user);
     public Iterable<Event> findEventsByEventTypeAndUser(int eventType, User user);
+    public Iterable<Event> findEventsByEventType(int eventType);
     public Iterable<Event> findEventsByDateBetween(LocalDate startDate, LocalDate endDate);
     public Iterable<Event> findEventsByDate(LocalDate date);
     //public Iterable<Event> findEventsByDateMonth(Month month);
