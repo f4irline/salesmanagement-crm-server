76211fa733561c53d30a7d5658464d4cd0f57d7b
jonizei <joni.koskinen@tuni.fi>
2019-03-28 13:31:57 +0200

    Admin can fetch users, leads and events

--- src/main/java/com/github/s1ckcode/SalesManagement/Admin/AdminController.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Admin/AdminController.java
@@ -1,19 +1,27 @@
 package com.github.s1ckcode.SalesManagement.Admin;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.github.s1ckcode.SalesManagement.Event.Event;
 import com.github.s1ckcode.SalesManagement.Event.EventRepository;
 import com.github.s1ckcode.SalesManagement.Lead.Lead;
 import com.github.s1ckcode.SalesManagement.Lead.LeadRepository;
 import com.github.s1ckcode.SalesManagement.User.User;
 import com.github.s1ckcode.SalesManagement.User.UserRepository;
+import com.github.s1ckcode.SalesManagement.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 public class AdminController {
+
+    @Autowired
+    private Utils utils;
 
     @Autowired
     private LeadRepository leadRepository;
@@ -24,19 +32,116 @@ public class AdminController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(value = "/admin/users")
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public enum ObjectType {
+        USER,
+        LEAD,
+        EVENT
+    }
+    
+    @GetMapping(value = "/admin")
+    public Iterable<ArrayNode> getAddData() {
+        Iterable<User> userList = userRepository.findAll();
+        Iterable<Lead> leadList = leadRepository.findAll();
+        Iterable<Event> eventList = eventRepository.findAll();
+
+        List<ArrayNode> arrayNodes = new ArrayList<>();
+        arrayNodes.add(createArrayNode(userList, ObjectType.USER));
+        arrayNodes.add(createArrayNode(leadList, ObjectType.LEAD));
+        arrayNodes.add(createArrayNode(eventList, ObjectType.EVENT));
+
+        return arrayNodes;
     }
 
-    @GetMapping(value = "/admin/leads")
-    public Iterable<Lead> getAllLeads() {
-        return leadRepository.findAll();
+    private ArrayNode createArrayNode(Iterable<?> list, ObjectType type) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode arrayNode = mapper.createArrayNode();
+
+        switch (type) {
+            case USER:
+
+                for(Object user : list) {
+                    arrayNode.add(createUserNode((User) user));
+                }
+
+                break;
+
+            case LEAD:
+
+                for(Object lead : list) {
+                    arrayNode.add(createLeadNode((Lead) lead));
+                }
+
+                break;
+
+            case EVENT:
+
+                for(Object event : list) {
+                    arrayNode.add(createEventNode((Event) event));
+                }
+
+                break;
+        }
+
+        return arrayNode;
     }
 
-    @GetMapping(value = "/admin/events")
-    public Iterable<Event> getAllEvents() {
-        return eventRepository.findAll();
+    private JsonNode createUserNode(User user) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.createObjectNode();
+        ((ObjectNode) node).put("user_id", user.getUserId());
+        ((ObjectNode) node).put("user_first",user.getUserFirst());
+        ((ObjectNode) node).put("user_last",user.getUserLast());
+        ((ObjectNode) node).put("hit_rate",utils.getHitrate(user));
+        ((ObjectNode) node).put("avg_sales",utils.getAvgSales(user));
+        ((ObjectNode) node).put("total_sales",utils.getAllSales(user));
+        ((ObjectNode) node).put("contacts_amount", ((List<Event>)eventRepository.findEventsByEventTypeAndUser(Event.CONTACT, user)).size());
+        ((ObjectNode) node).put("meetings_amount", ((List<Event>)eventRepository.findEventsByEventTypeAndUser(Event.MEETING, user)).size());
+        ((ObjectNode) node).put("offers_amount", ((List<Event>)eventRepository.findEventsByEventTypeAndUser(Event.OFFER, user)).size());
+        ((ObjectNode) node).put("sales_amount", ((List<Event>)eventRepository.findEventsByEventTypeAndUser(Event.SALE, user)).size());
+
+        return node;
+    }
+
+    private JsonNode createLeadNode(Lead lead) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.createObjectNode();
+        ((ObjectNode) node).put("lead_id", lead.getLeadId());
+        ((ObjectNode) node).put("lead_date", lead.getDate().toString());
+        ((ObjectNode) node).put("lead_company_name", lead.getCompanyName());
+        ((ObjectNode) node).put("lead_industry", lead.getIndustry());
+        ((ObjectNode) node).put("lead_contact_person", lead.getContactPerson());
+        ((ObjectNode) node).put("lead_contact_role", lead.getContactRole());
+        ((ObjectNode) node).put("lead_phonenumber", lead.getPhoneNumber());
+        ((ObjectNode) node).put("lead_email", lead.getEmail());
+        ((ObjectNode) node).put("lead_website", lead.getWebsite());
+        ((ObjectNode) node).put("lead_notes", lead.getNotes());
+
+        return node;
+    }
+
+    private JsonNode createEventNode(Event event) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.createObjectNode();
+        ((ObjectNode) node).put("event_id", event.getEventId());
+        ((ObjectNode) node).put("event_type", event.getEventType());
+        ((ObjectNode) node).put("event_notes", event.getNotes());
+        ((ObjectNode) node).put("event_date", event.getDate().toString());
+        ((ObjectNode) node).put("event_sum", event.getSum());
+        ((ObjectNode) node).put("event_place", event.getPlace());
+        ((ObjectNode) node).put("event_contact_person", event.getContactPerson());
+        ((ObjectNode) node).put("event_phonenumber", event.getPhoneNumber());
+        ((ObjectNode) node).put("event_email", event.getEmail());
+
+        return node;
+    }
+
+    @PostMapping(value = "/admin/users/add")
+    public void addUser(@RequestBody User user) {
+        userRepository.save(user);
     }
 
     @DeleteMapping(value = "/admin/users/{userId}")
