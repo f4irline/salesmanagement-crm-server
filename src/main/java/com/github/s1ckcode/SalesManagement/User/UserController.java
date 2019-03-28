51e039ba012f60ae4f9f21dec36e1d8b6331d0fb
VeeVee <ville-veikko.nieminen@cs.tamk.fi>
2019-03-28 13:17:55 +0200

    Change: Reordered variables in Event.java and moved one GetMapping method to UserController and deleted MyRestController

--- src/main/java/com/github/s1ckcode/SalesManagement/User/UserController.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/User/UserController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.s1ckcode.SalesManagement.Event.Event;
 import com.github.s1ckcode.SalesManagement.Event.EventRepository;
+import com.github.s1ckcode.SalesManagement.Lead.LeadRepository;
 import com.github.s1ckcode.SalesManagement.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,8 @@ public class UserController {
     UserRepository userRepository;
     @Autowired
     EventRepository eventRepository;
+    @Autowired
+    LeadRepository leadRepository;
     @Autowired
     Utils utils;
 
@@ -86,5 +89,20 @@ public class UserController {
         }
 
         return entities;
+    }
+
+    @GetMapping(value = "/userEvents/{userId}")
+    public Iterable<Iterable> getUserEvents(@PathVariable int userId) {
+        Iterable<Iterable> userEvents = new ArrayList<>();
+
+        User user = userRepository.findById(userId).get();
+
+        ((ArrayList<Iterable>) userEvents).add(eventRepository.findEventsByEventTypeAndUser(Event.CONTACT,user));
+        ((ArrayList<Iterable>) userEvents).add(eventRepository.findEventsByEventTypeAndUser(Event.MEETING,user));
+        ((ArrayList<Iterable>) userEvents).add(eventRepository.findEventsByEventTypeAndUser(Event.OFFER,user));
+        ((ArrayList<Iterable>) userEvents).add(eventRepository.findEventsByEventTypeAndUser(Event.SALE,user));
+        ((ArrayList<Iterable>) userEvents).add(leadRepository.findAll());
+
+        return userEvents;
     }
 }
