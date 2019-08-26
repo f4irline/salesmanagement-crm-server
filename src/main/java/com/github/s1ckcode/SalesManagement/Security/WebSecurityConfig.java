e330d8d5002943c169e159191f071090c86a0745
Tommi Lepola <lepola.tommi@gmail.com>
2019-08-26 18:02:01 +0300

    Fix origins and prepare for deployment

--- src/main/java/com/github/s1ckcode/SalesManagement/Security/WebSecurityConfig.java
+++ src/main/java/com/github/s1ckcode/SalesManagement/Security/WebSecurityConfig.java
@@ -62,8 +62,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())//.disable()
-                //.headers().frameOptions().disable()
+                .csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                     .and()
                 .requiresChannel()
                     .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
